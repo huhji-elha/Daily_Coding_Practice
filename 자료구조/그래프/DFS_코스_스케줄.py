0123737b9b6f28fe59d759922278df5420fb4b3e
huhji <huhji.elha@gmail.com>
2021-10-22 14:19:10 +0900

    add second solution of course schedule

--- 자료구조/그래프/DFS_코스_스케줄.py
+++ 자료구조/그래프/DFS_코스_스케줄.py
@@ -35,3 +35,36 @@ class Solution:
                 return False
 
         return True
+
+# 아이디어02 : 위의 방법대로 하면 이미 탐색한 노드를 여러번 반복해서 탐색하게된다.
+# 가령, [[0,2],[1,2],[2,3],[3,4]] 의 트리를 탐색한다고 했을때 0에서 시작하는 루트와 1에서 시작하는 루트가 같음에도 두 번 탐색하게 되는 것.
+# 따라서 이미 탐색을 마친 노드라는 것을 중간에 알면 탐색을 중단하고 True를 반환해야한다.
+
+
+class Solution2:
+    def canFinish(self, numCourses: int, prerequisites: list):
+        pre_dict = defaultdict(list)
+        for p in prerequisites:
+            pre_dict[p[0]] += [p[1]]
+
+        searched = set()  # 한번 방문한 노드 : 순환 구조 판별용
+        visited = set()  # 판별이 끝난 노드 : 재탐색 방지용
+
+        def dfs(node):
+            if node in searched:
+                return False
+            if node in visited:
+                return True
+            searched.add(node)
+            for i in pre_dict[node]:
+                if not dfs(i):
+                    return False
+            # 새로 탐색하는 노드가 순환 구조가 아닐 때 여기로 넘어온다.
+            searched.remove(node)  # 한번 방문한 노드는 삭제한다.
+            visited.add(node)  # 순환 구조가 아닌 노드는 저장한다.
+            return True
+
+        for i in list(pre_dict):  # 트리의 시작점이 여러 곳일 수 있으므로 for문으로 전체 탐색한다.
+            if not dfs(i):
+                return False
+        return True
