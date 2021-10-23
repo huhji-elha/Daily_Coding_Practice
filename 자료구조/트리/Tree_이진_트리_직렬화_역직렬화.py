395c787259ba15f296944732f03250033d32d157
huhji <huhji.elha@gmail.com>
2021-10-23 21:39:35 +0900

    add bt serialize and deserialize

---
+++ 자료구조/트리/Tree_이진_트리_직렬화_역직렬화.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+이진 트리를 배열로 직렬화하고, 반대로 역직렬화하는 기능 구현.
+
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+"""
