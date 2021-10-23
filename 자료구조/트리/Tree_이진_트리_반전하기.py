d0e89b7de78145b4003904a8e275fef16c6c2589
huhji <huhji.elha@gmail.com>
2021-10-23 19:47:27 +0900

    add runtime and memory on invert bt

--- 자료구조/트리/Tree_이진_트리_반전하기.py
+++ 자료구조/트리/Tree_이진_트리_반전하기.py
@@ -17,7 +17,8 @@ class TreeNode:
 # 주의사항 : 노드 값을 뒤집는 것이 아니라 트리 구조 자체를 뒤집는 것이다. -> root를 리턴해야한다.
 import collections
 
-
+# Runtime : 36 ms
+# Memory : 14 MB
 class Solution:
     def invertTree(self, root: TreeNode):
         queue = collections.deque([root])
@@ -33,27 +34,28 @@ class Solution:
 
 # 아이디어 02 : 재귀함수로 트리 뒤집기
 # 트리는 서브트리의 집합이기 때문에 이러한 방식이 가능하다.
-
-
+# Runtime : 58 ms
+# Memory : 14.3 MB
 class Solution2:
     def invertTree(self, root: TreeNode):
-        root.left, root.right = (
-            self.invertTree(root.right),
-            self.invertTree(root.left),
-        )
-        return root
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None  # 생략 가능하다. 파이썬은 아무것도 리턴하지 않으면 자연스럽게 None을 리턴한다.
 
 
 # 아이디어 03 : DFS로 풀기
 # 위의 BFS는 popleft를 써서 가장 먼저 들어온 값을 먼저 빼내면서 큐로 풀이했다.
 # DFS로 풀면 stack 구조로 가장 나중에 들어온 값을 먼저 빼내면 된다.
+# Runtime : 33 ms
+# Memory : 14.1 MB
 class Solution3:
     def invertTree(self, root: TreeNode):
         stack = collections.deque([root])
         while stack:
             node = stack.pop()
             if node:
-                node.left, node.right = node.left, node.right
+                node.left, node.right = node.right, node.left
                 stack.append(node.left)
                 stack.append(node.right)
         return root
